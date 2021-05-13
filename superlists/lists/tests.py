4b5871a7a087351015c385492d9403749e047e83
nervig <solide@yandex.ru>
2021-05-13 14:35:29 -0400

    The first unit test and url mapping, dummy representation

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,9 +1,12 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
 
-class SmokeTest(TestCase):
-    '''test of toxic'''
+class HomePageTest(TestCase):
+    '''test of home page'''
 
-    def test_bad_maths(self):
-        '''incorrect math calculation'''
-        self.assertEqual(1 + 1, 3)
+    def test_root_url_resolves_to_home_page_view(self):
+        '''test: root url converted to image of home page'''
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
 
