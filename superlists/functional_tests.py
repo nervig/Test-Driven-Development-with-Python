18cdb6348995649f96dba25e49febe1c7eb1a86a
nervig <solide@yandex.ru>
2021-05-29 10:46:57 -0400

    Basic view now returns minimal HTML

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,63 +1,41 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import time
 import unittest
 
 
 class NewVisitorTest(unittest.TestCase):
-    '''a test of new visitor'''
+    ''' test of new user '''
 
     def setUp(self):
-        '''setup'''
+        ''' setup '''
         self.browser = webdriver.Firefox()
 
     def tearDown(self):
-        '''retrieve'''
+        ''' tear down '''
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        '''test: can start a list and get his it later'''
-        # Elin heard about cool new online-app with
-        # list of urgent tasks. She decided to estimate his home page.
-        self.browser.get('http:/localhost:8000')
+        # Eith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
 
-        # She sees, what a title and header tell about lists urgent tasks.
+        # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-Do', header_text)
+        self.fail('End the test!')
 
-        # To her immediately is suggested to enter a unit of list
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
-        )
+    # She is invited to enter a to-do item straight away
+    # She types "Buy peacock feathers" into a text box (Edith's hobby
+    # is tying fly-fishing lures)
+    # When she hits enter, the page updates, and now the page lists
+    # "1: Buy peacock feathers" as an item in a to-do list
+    # There is still a text box inviting her to add another item. She
+    # enters "Use peacock feathers to make a fly" (Edith is very methodical)
+    # The page updates again, and now shows both items on her list
+    # Edith wonders whether the site will remember her list. Then she sees
+    # that the site has generated a unique URL for her -- there is some
+    # explanatory text to that effect.
+    # She visits that URL - her to-do list is still there.
+    # Satisfied, she goes back to sleep
 
-        # She types in a text field "Buy a new flash drive"
-        inputbox.send_keys('Buy flash drive')
 
-        # When she push on enter, the page is reloading, and now the page contains
-        # "1: Buy a new flash drive" as a unit of item of list
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy flash drive' for row in rows)
-        )
-        self.fail('Finish the test!')
-# The field of text still invites her to add yet one element.
-# She enters "To make a write on flash drive"
-
-# The page again reload and now shows both elements of her list.
-
-# Elin be interesting, the site will remember the list of her? Then she sees,
-# that the site has generated the unical url-address for her -
-# about this it is showing a little text with explains.
-
-# She visits this URL-address - it is list of she still there.
-
-# Satisfied with the result, she go to the bed.
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,18 +1,17 @@
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
+
 from lists.views import home_page
 
+
 class HomePageTest(TestCase):
-    '''test of home page'''
 
     def test_root_url_resolves_to_home_page_view(self):
-        '''test: root url converted to image of home page'''
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
-        '''test: the home page returns correct html'''
         request = HttpRequest()
         response = home_page(request)
         html = response.content.decode('utf8')
